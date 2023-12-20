35494467406f445b177add438c8a1ff08ad54f06
blackshinemusic <144614162+blackshinemusic@users.noreply.github.com>
2023-12-20 21:59:38 +0300

    Update Init.java

--- src/main/java/ru/kata/spring/boot_security/demo/Init/Init.java
+++ src/main/java/ru/kata/spring/boot_security/demo/Init/Init.java
@@ -2,6 +2,7 @@ package ru.kata.spring.boot_security.demo.Init;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
@@ -14,34 +15,42 @@ import java.util.Set;
 public class Init implements ApplicationListener<ContextRefreshedEvent> {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder encoder;
 
-    public Init(UserRepository userRepository, RoleRepository roleRepository) {
+    public Init(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder encoder) {
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
+        this.encoder = encoder;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        Role adminRole = new Role();
+        adminRole.setRoleName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
 
         Role userRole = new Role();
         userRole.setRoleName("ROLE_USER");
         roleRepository.save(userRole);
 
-        Role adminRole = new Role();
-        adminRole.setRoleName("ROLE_ADMIN");
-        roleRepository.save(adminRole);
-
         Set<Role> userRoles = Set.of(userRole);
         Set<Role> adminRoles = Set.of(adminRole, userRole);
 
-        User user = new User(
-                "vova",
-                "$2a$12$1TrhXtZcJ1M/r5g0rlBQ3ecFKK1kXdGJyFzwDPrB3nRA8/iGFFmqy",
-                "wefwf",
-                24,
-                adminRoles);
-        userRepository.save(user);
+        User admin = new User();
+        admin.setUsername("admin");
+        admin.setPassword(encoder.encode("admin"));
+        admin.setAge(5);
+        admin.setEmail("ad@ad.com");
+        admin.setRoles(adminRoles);
+        userRepository.save(admin);
 
+        User user = new User();
+        user.setUsername("user");
+        user.setPassword(encoder.encode("user"));
+        user.setAge(5);
+        user.setEmail("us@us.com");
+        user.setRoles(userRoles);
+        userRepository.save(user);
 
     }
 }
